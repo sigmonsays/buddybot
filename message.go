5d2eebf5ccc60ed459464a436c6c82519435bae2
Sig Lange <sig.lange@gmail.com>
2017-01-02 12:41:12 -0800

    omit empty from & message

--- message.go
+++ message.go
@@ -40,12 +40,12 @@ type Message struct {
 	connection *Connection
 	Id         int64  `json:"id"`
 	Op         OpCode `json:"op"`
-	From       string `json:"from"`
-	Message    string `json:"message"`
+	From       string `json:"from,omitempty"`
+	Message    string `json:"message,omitempty"`
 }
 
 func (m *Message) String() string {
-	return fmt.Sprintf("cid:%d op:%s/%d from:%s message:%s",
+	return fmt.Sprintf("cid:%d op:%s/%d from:%q message:%q",
 		m.Id, m.Op, m.Op, m.From, m.Message)
 }
 func (m *Message) Json() []byte {
