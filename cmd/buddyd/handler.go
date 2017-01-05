0bd32584742680bf3b05b4a9be8f2644fa6bc256
Sig Lange <sig.lange@gmail.com>
2017-01-05 18:20:33 -0500

    fix up the handler to use a function for each operation

--- cmd/buddyd/handler.go
+++ cmd/buddyd/handler.go
@@ -51,49 +51,86 @@ func (h *chatHandler) getHistory() []*buddybot.Message {
 }
 
 func (h *chatHandler) handleMessage(op buddybot.OpCode, hub *buddybot.Hub, c *buddybot.Connection, m *buddybot.Message) error {
-
 	if op == buddybot.MessageOp {
-		log.Debugf("handleMessage op:%s/%d msg:%s", op, op, m)
-		hub.SendBroadcast(m)
+		h.handleMessageOp(op, hub, c, m)
 
 	} else if op == buddybot.RegisterOp {
+		h.handleRegisterOp(op, hub, c, m)
 
 	} else if op == buddybot.UnregisterOp {
-		hub.Send(buddybot.NoticeOp, fmt.Sprintf("%s has left", c.Identity))
+		h.handleUnregisterOp(op, hub, c, m)
 
 	} else if op == buddybot.JoinOp {
-
-		if m.From == "" {
-			log.Warnf("Join without a name (From not set)")
-			return nil
-		}
-
-		c.Identity = m.From
-		log.Infof("connection %d is now known as %q", c.GetId(), m.From)
-		hub.SendBroadcast(m)
+		h.handleJoinOp(op, hub, c, m)
 
 	} else if op == buddybot.NickOp {
-
-		if c.Identity == "" {
-			hub.Send(buddybot.NoticeOp, fmt.Sprintf("%s has joined", m.From))
-		} else {
-			hub.Send(buddybot.NoticeOp, fmt.Sprintf("%s has changed their name to %s", c.Identity, m.From))
-		}
-		c.Identity = m.From
+		h.handleNickOp(op, hub, c, m)
 
 	} else if op == buddybot.PingOp {
+		h.handlePingOp(op, hub, c, m)
 
 	} else if op == buddybot.NoticeOp {
-		hub.SendBroadcast(m)
+		h.handleNoticeOp(op, hub, c, m)
 
 	} else if op == buddybot.ClientListOp {
-		err := hub.SendClientList(m.Id)
-		if err != nil {
-			log.Infof("ClientList: %s", err)
-		}
+		h.handleClientListOp(op, hub, c, m)
 
 	} else {
 		log.Infof("Unhandled op %s/%d: %+v", m.Op, m.Op, m)
+	}
+	return nil
+}
+
+func (h *chatHandler) handleMessageOp(op buddybot.OpCode, hub *buddybot.Hub, c *buddybot.Connection, m *buddybot.Message) error {
+	log.Debugf("handleMessage op:%s/%d msg:%s", op, op, m)
+	hub.SendBroadcast(m)
+	return nil
+}
+
+func (h *chatHandler) handleUnregisterOp(op buddybot.OpCode, hub *buddybot.Hub, c *buddybot.Connection, m *buddybot.Message) error {
+	return nil
+}
+
+func (h *chatHandler) handleRegisterOp(op buddybot.OpCode, hub *buddybot.Hub, c *buddybot.Connection, m *buddybot.Message) error {
+	hub.Send(buddybot.NoticeOp, fmt.Sprintf("%s has left", c.Identity))
+	return nil
+}
+
+func (h *chatHandler) handleJoinOp(op buddybot.OpCode, hub *buddybot.Hub, c *buddybot.Connection, m *buddybot.Message) error {
+	if m.From == "" {
+		log.Warnf("Join without a name (From not set)")
+		return nil
+	}
+
+	c.Identity = m.From
+	log.Infof("connection %d is now known as %q", c.GetId(), m.From)
+	hub.SendBroadcast(m)
+	return nil
+}
+
+func (h *chatHandler) handleNickOp(op buddybot.OpCode, hub *buddybot.Hub, c *buddybot.Connection, m *buddybot.Message) error {
+	if c.Identity == "" {
+		hub.Send(buddybot.NoticeOp, fmt.Sprintf("%s has joined", m.From))
+	} else {
+		hub.Send(buddybot.NoticeOp, fmt.Sprintf("%s has changed their name to %s", c.Identity, m.From))
+	}
+	c.Identity = m.From
+	return nil
+}
+
+func (h *chatHandler) handlePingOp(op buddybot.OpCode, hub *buddybot.Hub, c *buddybot.Connection, m *buddybot.Message) error {
+	return nil
+}
+
+func (h *chatHandler) handleNoticeOp(op buddybot.OpCode, hub *buddybot.Hub, c *buddybot.Connection, m *buddybot.Message) error {
+	hub.SendBroadcast(m)
+	return nil
+}
+
+func (h *chatHandler) handleClientListOp(op buddybot.OpCode, hub *buddybot.Hub, c *buddybot.Connection, m *buddybot.Message) error {
+	err := hub.SendClientList(m.Id)
+	if err != nil {
+		log.Infof("ClientList: %s", err)
 	}
 	return nil
 }
