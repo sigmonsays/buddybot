bf110602fbf9869f55d97016a78b78f232d05c86
Sig Lange <sig.lange@gmail.com>
2017-01-07 09:52:12 -0800

    add arg func

--- cmd/buddy/line.go
+++ cmd/buddy/line.go
@@ -33,12 +33,18 @@ func ParseCommandLine(line string) (*CommandLine, error) {
 
 	return cl, nil
 }
+func (me *CommandLine) Arg(i int) string {
+	if i < len(me.Args) {
+		return me.Args[i]
+	}
+	return ""
+}
 
 func (me *CommandLine) SliceArgs(offset int) []string {
-	// log.Tracef("slice args len=%d offset=%d", len(me.Args), offset)
+	log.Tracef("slice args len=%d offset=%d", len(me.Args), offset)
 	if len(me.Args) >= offset {
 		me.Args = me.Args[offset:]
-		// log.Debugf("new args (len %d) %q", len(me.Args), me.Args)
+		log.Debugf("new args (len %d) %q", len(me.Args), me.Args)
 	}
 	return me.Args
 }
