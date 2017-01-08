9838d31d24a1589b0a64aca3b8af6077382f7a22
Sig Lange <sig.lange@gmail.com>
2017-01-07 20:03:47 -0500

    add server generated messages

--- cmd/buddyd/server_command.go
+++ cmd/buddyd/server_command.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/sigmonsays/buddybot"
@@ -33,6 +34,15 @@ func parseCommand(line string) ([]string, error) {
 	return args, nil
 }
 
+func (h *chatHandler) sendNotice(hub *buddybot.Hub, c *buddybot.Connection, s string, args ...interface{}) error {
+	m := &buddybot.Message{
+		Op:      buddybot.NoticeOp,
+		Message: fmt.Sprintf(s, args...),
+	}
+	hub.SendTo(c, m)
+	return nil
+}
+
 func (h *chatHandler) handleServerCommand(op buddybot.OpCode, hub *buddybot.Hub, c *buddybot.Connection, m *buddybot.Message) error {
 	log.Debugf("handleServerCommand %s/%d msg:%s", op, op, m)
 
@@ -51,14 +61,29 @@ func (h *chatHandler) handleServerCommand(op buddybot.OpCode, hub *buddybot.Hub,
 	if cmd == "who" {
 		hub.SendClientList(c.GetId())
 
+	} else if cmd == "list-opcodes" {
+		for _, opcode := range buddybot.OpCodes() {
+			h.sendNotice(hub, c, "opcode %d %s", opcode, opcode)
+		}
+
+	} else if cmd == "send-message" {
+
+		m := buddybot.NewMessage()
+
+		opcode, err := strconv.Atoi(args[1])
+		if err != nil {
+			return err
+		}
+		m.Op = buddybot.OpCode(opcode)
+		m.From = args[2]
+		m.To = args[3]
+		m.Message = args[4]
+		log.Infof("Sending server generated message %s", m)
+		hub.SendBroadcast(m)
+
 	} else {
 		log.Warnf("unknown command: cmd=%s", args)
-
-		m := &buddybot.Message{
-			Op:      buddybot.NoticeOp,
-			Message: fmt.Sprintf("No such command: %s", cmd),
-		}
-		hub.SendTo(c, m)
+		h.sendNotice(hub, c, "No such command: %s", cmd)
 	}
 
 	return nil
